15e3b28f7029d7920db375427ae6999b7ed91a7d
Lucas Fernando Soares Morgado de Souza <lucasmorgaadoo@gmail.com>
2023-07-08 02:24:23 -0300

    changes into the menu and some ui

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -15,4 +16,30 @@ public class MainMenu : MonoBehaviour
     {
         
     }
+
+    public void StartGame ()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+
+    public void LucasMorgado ()
+    {
+        Application.OpenURL("https://lucasmorgado.itch.io");
+    }
+
+    public void LuizFelipe ()
+    {
+        Application.OpenURL("https://luizfelipemb.itch.io");
+    }
+
+    public void Mounirtohami ()
+    {
+        Application.OpenURL("https://mounirtohami.itch.io");
+    }
+
+    public void Quit ()
+    {
+        Application.Quit();
+    }
+
 }
